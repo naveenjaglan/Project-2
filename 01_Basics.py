0c92b869561282f8b880885f5afa01e03c1629e6
naveenjaglan <naveenjaglan1@gmail.com>
2020-02-20 20:26:39 +0530

    test

--- 01_Basics.py
+++ 01_Basics.py
@@ -1,1 +1,41 @@
-Print('Hello')
+# some operations in numbers
+print(2+2)
+
+print(8%4)
+print(4/2)
+
+# here we got 2.0 not 2 because it give us float which means floating point representation, in which "." used after numbers.
+# if you divide 5 by 2 then you want 2.5 not 2 so it convert this int into float to give you 2.5 as output then 2.0.
+
+# if you want answer in integer than use double slash.
+print(5//2)
+
+print(8+9-10)
+print(8+2-7+(0+30-12))
+
+# cube function
+print(2**12)
+
+# Mod operator: gives the remainder.
+print(10%3)
+
+'''
+String- Combination of characters, you can use double quote or single quote to write string.
+'''
+
+print("navin")
+print('navin')
+print('navin\' ')
+print('navin\'s laptop')
+
+print(10*"navin")
+print("c:\docs\navin")
+
+#raw string - removes all functions
+
+print(r'c:\docs\navin')
+
+
+# how to write backslash in python
+
+print("2\\2")
